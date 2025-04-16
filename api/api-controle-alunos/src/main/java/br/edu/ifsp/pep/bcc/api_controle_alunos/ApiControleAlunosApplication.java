850e7098249e4cbc8c512b3e24043dba4a3fbc3d
Vongui <guilhermemoraes8@hotmail.com>
2025-04-16 16:47:12 -0300

    feat: add base project-api

---
+++ api/api-controle-alunos/src/main/java/br/edu/ifsp/pep/bcc/api_controle_alunos/ApiControleAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.pep.bcc.api_controle_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleAlunosApplication.class, args);
+	}
+
+}
---
+++ api/api-controle-alunos/src/test/java/br/edu/ifsp/pep/bcc/api_controle_alunos/ApiControleAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.pep.bcc.api_controle_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

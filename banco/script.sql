0a03d8f77fcc2fd03f7c6010b5981970120eed40
JoaoPSFranca <francasjoaopedro@gmail.com>
2025-04-17 09:56:12 -0300

    fix: translate database

--- banco/script.sql
+++ banco/script.sql
@@ -2,67 +2,67 @@ DROP DATABASE IF EXISTS controleAluno;
 CREATE DATABASE controleAluno;
 USE controleAluno;
 
-CREATE TABLE horario (
-	horario_id INT NOT NULL AUTO_INCREMENT,
-    horario_inicio TIME NOT NULL,
-    horario_fim TIME NOT NULL,
-    horario_periodo ENUM('matutino', 'vespertino', 'noturno') NOT NULL,
-    PRIMARY KEY (horario_id)
+CREATE TABLE schedule (
+	schedule_id INT NOT NULL AUTO_INCREMENT,
+    schedule_start TIME NOT NULL,
+    schedule_end TIME NOT NULL,
+    schedule_period ENUM('matutino', 'vespertino', 'noturno') NOT NULL,
+    PRIMARY KEY (schedule_id)
 );
 
-CREATE TABLE turma (
-	turma_id INT NOT NULL AUTO_INCREMENT,
-    turma_sigla VARCHAR(20) NOT NULL,
-    turma_ano YEAR NOT NULL,
-    PRIMARY KEY (turma_id)
+CREATE TABLE class (
+	class_id INT NOT NULL AUTO_INCREMENT,
+    class_acronym VARCHAR(20) NOT NULL,
+    class_year YEAR NOT NULL,
+    PRIMARY KEY (class_id)
 );
 
-CREATE TABLE aluno (
-	aluno_id INT NOT NULL AUTO_INCREMENT,
-    turma_id INT NOT NULL,
-    aluno_nome VARCHAR(100) NOT NULL,
-    aluno_data_nascimento DATE NOT NULL,
-    aluno_maioridade TINYINT(1) DEFAULT '0',
-    aluno_permissao ENUM('total', 'parcial', 'negada') DEFAULT 'negada',
-    PRIMARY KEY (aluno_id),
-    FOREIGN KEY (turma_id) REFERENCES turma (turma_id)
+CREATE TABLE student (
+	student_id INT NOT NULL AUTO_INCREMENT,
+    class_id INT NOT NULL,
+    student_name VARCHAR(100) NOT NULL,
+    student_birthdate DATE NOT NULL,
+    student_adult TINYINT(1) DEFAULT '0',
+    student_license ENUM('total', 'parcial', 'negada') DEFAULT 'negada',
+    PRIMARY KEY (student_id),
+    FOREIGN KEY (class_id) REFERENCES class (class_id)
 );
 
-CREATE TABLE aluno_permissoes (
-	permissao_id INT NOT NULL,
-    aluno_id INT NOT NULL,
-    horario_permitido TIME NOT NULL,
-    PRIMARY KEY (permissao_id, aluno_id),
-    FOREIGN KEY (aluno_id) REFERENCES aluno (aluno_id)
+CREATE TABLE student_license (
+	license_id INT NOT NULL,
+    student_id INT NOT NULL,
+    allowed_time TIME NOT NULL,
+    PRIMARY KEY (license_id, student_id),
+    FOREIGN KEY (student_id) REFERENCES student (student_id)
 );
 
-CREATE TABLE responsavel (
-	responsavel_id INT NOT NULL AUTO_INCREMENT,
-    aluno_id INT NOT NULL,
-    responsavel_nome VARCHAR(100) NOT NULL,
-    responsavel_data_nascimento DATE NOT NULL,
-    PRIMARY KEY (responsavel_id),
-    FOREIGN KEY (aluno_id) REFERENCES aluno (aluno_id)
+CREATE TABLE guardian (
+	guardian_id INT NOT NULL AUTO_INCREMENT,
+    student_id INT NOT NULL,
+    guardian_name VARCHAR(100) NOT NULL,
+    guardian_birthdate DATE NOT NULL,
+    PRIMARY KEY (guardian_id),
+    FOREIGN KEY (student_id) REFERENCES student (student_id)
 );
 
-CREATE TABLE grade (
-	grade_dia ENUM('segunda', 'terca', 'quarta', 'quinta', 'sexta') NOT NULL,
-    horario_id INT NOT NULL,
-    turma_id INT NOT NULL,
-    PRIMARY KEY (grade_dia, horario_id, turma_id),
-    FOREIGN KEY (horario_id) REFERENCES horario (horario_id),
-    FOREIGN KEY (turma_id) REFERENCES turma (turma_id)
+CREATE TABLE timetable (
+	timetable_day ENUM('segunda', 'terca', 'quarta', 'quinta', 'sexta') NOT NULL,
+    schedule_id INT NOT NULL,
+    class_id INT NOT NULL,
+    PRIMARY KEY (timetable_day, schedule_id, class_id),
+    FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id),
+    FOREIGN KEY (class_id) REFERENCES class (class_id)
 );
 
-CREATE TABLE relatorio (
-	relatorio_id INT NOT NULL,
-    aluno_id INT NOT NULL,
-    relatorio_data DATE NOT NULL,
-    relatorio_horario TIME NOT NULL,
-    responsavel_id INT,
-    relatorio_motivo TEXT,
-    relatorio_documento TEXT,
-    PRIMARY KEY (relatorio_id),
-    FOREIGN KEY (aluno_id) REFERENCES aluno (aluno_id),
-    FOREIGN KEY (responsavel_id) REFERENCES responsavel (responsavel_id)
+CREATE TABLE report (
+	report_id INT NOT NULL,
+    student_id INT NOT NULL,
+    report_date DATE NOT NULL,
+    report_time TIME NOT NULL,
+    guardian_id INT,
+    report_reason TEXT,
+    report_doc TEXT,
+    PRIMARY KEY (report_id),
+    FOREIGN KEY (student_id) REFERENCES student (student_id),
+    FOREIGN KEY (guardian_id) REFERENCES guardian (guardian_id)
 );
